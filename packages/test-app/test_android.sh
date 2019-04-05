7c3c37fa06e7216cd0c74e5fb3f257f4202f0ae1
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2019-04-05 18:23:51 +0200

    chore: force android exit code

--- packages/test-app/test_android.sh
+++ packages/test-app/test_android.sh
@@ -6,7 +6,12 @@ pushd project
     yarn react-native link
 popd
 
+echo 'Launching Detox'
 yarn detox build --configuration android
+
+echo 'Before exit(2)'
+exit 2
+echo 'After exit(2)'
 
 # Android E2E tests are not working yet
 # yarn detox test --configuration android
